1cc96c700a06a72d3e494db009a3d1154bb63f15
Ash <davicaporal123@hotmail.com>
2024-07-10 14:07:55 -0300

    custom global physics, density and stroke size controls

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
+let densityInput = document.getElementById('density');
+let strokeSizeInput = document.getElementById('stroke');
+let strokeNumber = document.getElementById('strokeNumber');
 
-let height = 500;
-let width = 500;
-let strokeSize = 100;
+let height = 100;
+let width = 100;
+let strokeSize = 20;
+let gravity = { x: 1, y: 1 };
 
 function createMatrix(width, height) {
     let matrix = new Array(width);
@@ -23,65 +27,17 @@ function createMatrix(width, height) {
 let matrix = createMatrix(width, height);
 
 function update() {
-    //Physics
-    let newMatrix = createMatrix(width, height); //Create new matrix to apply the new cordinates
+    let newMatrix = createMatrix(width, height);
     for (let x = 0; x < matrix.length; x++) {
         for (let y = 0; y < matrix[x].length; y++) {
             if (matrix[x][y] === 1) {
-                //Check if theres empty space below
-                if (matrix[x][y + 1] === 0) {
-                    newMatrix[x][y + 1] = 1;
+                let newX = x + gravity.x;
+                let newY = y + gravity.y;
+
+                if (newX >= 0 && newX < width && newY >= 0 && newY < height && matrix[newX][newY] === 0) {
+                    newMatrix[newX][newY] = 1;
                 } else {
-
-                    //Check if a collection of frags are in midair, if not, slide to the side
-                    let slide = false;
-                    for (let i = y; i < height; i++) {
-                        if (matrix[x][i] === 0) {
-                            slide = false;
-                            break;
-                        }
-                        if (i === height - 1) {
-                            slide = true;
-                        }
-                    }
-
-                    let left = 0;
-                    let right = 0;
-                    if (slide === true) {
-                        //Check if theres empty space to the right
-                        right = 0;
-                        if (x + 1 <= width - 1) {
-                            if (matrix[x + 1][y + 1] === 0) {
-                                right = 1;
-                            }
-                        }
-
-                        //Check if theres empty space to the left
-                        left = 0;
-                        if (x - 1 >= 0) {
-                            if (matrix[x - 1][y + 1] === 0) {
-                                left = 1;
-                            }
-                        }
-
-                        if (left === 1 && right === 1) { //if both sides are available, choose randomly
-                            if (Math.random() > 0.5) {
-                                left = 1;
-                                right = 0;
-                            } else {
-                                left = 0;
-                                right = 1;
-                            }
-                        }
-                    }
-
-                    if (left === 1) {
-                        newMatrix[x - 1][y + 1] = 1;
-                    } else if (right === 1) {
-                        newMatrix[x + 1][y + 1] = 1;
-                    } else {
-                        newMatrix[x][y] = 1;
-                    }
+                    newMatrix[x][y] = 1;
                 }
             }
         }
@@ -89,7 +45,6 @@ function update() {
 
     matrix = newMatrix;
 
-    //Draw
     for (let x = 0; x < matrix.length; x++) {
         for (let y = 0; y < matrix[x].length; y++) {
             switch (matrix[x][y]) {
@@ -100,13 +55,11 @@ function update() {
                     ctx.fillStyle = 'black';
                     break;
             }
-
             ctx.fillRect(x * (canvas.width / width), y * (canvas.height / height), canvas.width / width, canvas.height / height);
         }
     }
 }
 
-//Spawn square
 function square(x, y, size) {
     let cornerX = x - size / 2;
     let cornerY = y - size / 2;
@@ -121,24 +74,119 @@ function square(x, y, size) {
     }
 }
 
-//Get mouse pos in canvas
-function getMouse(canvas, e) {
-    var rect = canvas.getBoundingClientRect();
+function getMouse(element, e) {
+    var rect = element.getBoundingClientRect();
     return [e.clientX - rect.left, e.clientY - rect.top];
 }
 
-//Canvas click event
 canvas.addEventListener('click', (e) => {
     let [mX, mY] = getMouse(canvas, e);
 
-    //Normalize to set width x height
     mX = Math.min(Math.round((mX / canvas.width) * width), width - 1);
     mY = Math.min(Math.round((mY / canvas.width) * height), height - 1);
 
     square(mX, mY, strokeSize);
 })
 
+canvas.addEventListener('mousemove', (e) => {
+    let mX = e.clientX;
+    let mY = e.clientY;
+    
+    mX -= strokeSize * (canvas.width / width) / 2;
+    mY -= strokeSize * (canvas.height / height) / 2;
+
+    let strokeIndicators = document.getElementsByClassName('strokeIndicator');
+    for (let i = 0; i < strokeIndicators.length; i++) {
+        strokeIndicators[i].style.display = 'block';
+        strokeIndicators[i].style.left = mX + 'px';
+        strokeIndicators[i].style.top = mY + 'px';
+        strokeIndicators[i].style.width = (strokeSize * (canvas.width / width)) + 'px';
+        strokeIndicators[i].style.height = (strokeSize * (canvas.height / height)) + 'px';
+    }
+})
+
+canvas.addEventListener('mouseleave', (e) => {
+    let strokeIndicators = document.getElementsByClassName('strokeIndicator');
+    for (let i = 0; i < strokeIndicators.length; i++) {
+        strokeIndicators[i].style.display = 'none';
+    }
+})
+
+canvas.addEventListener('wheel', (e) => {
+    strokeSize += Math.sign(e.deltaY);
+    strokeSize = Math.max(1, strokeSize);
+    strokeSize = Math.min(height, strokeSize);
+
+    let mX = e.clientX;
+    let mY = e.clientY;
+    
+    mX -= strokeSize * (canvas.width / width) / 2;
+    mY -= strokeSize * (canvas.height / height) / 2;
+
+    let strokeIndicators = document.getElementsByClassName('strokeIndicator');
+    for (let i = 0; i < strokeIndicators.length; i++) {
+        strokeIndicators[i].style.display = 'block';
+        strokeIndicators[i].style.left = mX + 'px';
+        strokeIndicators[i].style.top = mY + 'px';
+        strokeIndicators[i].style.width = (strokeSize * (canvas.width / width)) + 'px';
+        strokeIndicators[i].style.height = (strokeSize * (canvas.height / height)) + 'px';
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (gravity.y > -1) {
+                gravity.y--;
+            }
+            break;
+        case 'ArrowDown':
+            if (gravity.y < 1) {
+                gravity.y++;
+            }
+            break;
+        case 'ArrowLeft':
+            if (gravity.x > -1) {
+                gravity.x--;
+            }
+            break;
+        case 'ArrowRight':
+            if (gravity.x < 1) {
+                gravity.x++;
+            }
+            break;
+    }
+
+    updateGravArrow();
+})
+
+densityInput.addEventListener('input', updateDensity);
+
+function updateGravArrow() {
+    let arrow = document.getElementById('gravityArrow').children[2];
+    let circle = document.getElementById('gravityArrow').children[1];
+    if (gravity.x === 0 && gravity.y === 0) {
+        circle.style.display = '';
+        arrow.style.display = 'none';
+    } else {
+        circle.style.display = 'none';
+        arrow.style.display = '';
+
+        let angle = (Math.atan2(gravity.y, gravity.x) * 180 / Math.PI) + 90;
+        arrow.setAttribute('transform', `rotate(${angle}, 25, 25)`);
+    }
+}
+
+function updateDensity() {
+    height = parseInt(densityInput.value);
+    width = parseInt(densityInput.value);
+
+    matrix = createMatrix(width, height);
+}
+
+
 //Loop
+updateGravArrow();
 window.requestAnimationFrame(gameLoop);
 
 function gameLoop() {
